d4c5a3b199a991d8de6a536629cda7c7436e8775
Sue Roberts <suerobertstx@yahoo.com>
2022-12-08 15:16:01 -0600

    Add img & style Header

--- dentist/src/components/header/Header.jsx
+++ dentist/src/components/header/Header.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import tempImage from "../../assets/images/image1.png";
+import './header.scss'
+
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <section className="section-1 header__container section__padding">
+      <main className="intro-page">
+        <div classname="container">
+          <h1>Tillamook Family Dentistry</h1> 
+          <h2>2503 Main Ave NTillamook, OR 97141</h2>
+          <img src={tempImage} alt="temp image" width='40%'/>
+        </div>
+
+        
+      </main>
+    
+    </section>
   )
 }
 
