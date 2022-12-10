81a4718907d2a45499c486cf91addd9124130fe3
Sue Roberts <suerobertstx@yahoo.com>
2022-12-10 14:27:39 -0600

    Add image & articles to About Us

--- dentist/src/pages/about/About.jsx
+++ dentist/src/pages/about/About.jsx
@@ -1,22 +1,34 @@
 import React from 'react'
-
+import Fade from 'react-reveal/Fade'
 import './about.scss'
+import drImage from "../../assets/images/drAhn.jpg";
+import smileImage from "../../assets/images/tooth.jpeg";
+
 
 const About = () => {
   return (
-<>
+  <>
 
-    <div className="section-about-wrapper section__padding">
-      
+  <div className="section-about-wrapper section__padding">
+    <h1>About Us</h1> 
       <article className="section-about-description">
-      <div classname="container">
-      <h1>About Us</h1> 
-      <br />
-      <h2> Meet Dr. Jin Ahn DMD</h2>
-      <br />
-      </div>
+  
+        <Fade>  
+          <img src={drImage } className="drAhn-Img" alt="temp" width='20%' />
+          <h2>Meet Dr. Jin Ahn DMD</h2>
+          <p > Dr. Ahn attended the University of Dentistry in Illinois, where he graduated with honors. Returning to St Louis, he completed a residency at Saint Louis Hospital, where he received advanced training in emergency general dentistry. Since his residency, he has been practicing in the Oregon Coast area since 1994.
+          </p>
+        </Fade>
+
+        <Fade bottom >
+          <img src={smileImage } className="drAhn-Img" alt="temp" width='20%'  />
+          <h2>Our Philosophy</h2>
+          <p className="sec-2"> Tillamook Family Dentistry is here to provide you with a dental experience marked by gentle, compassionate and economic care. We provide  one on one approach to our patients. We work with very caring and professional specialists to provide you the best dental experience as possible to our patients. Our experienced and friendly dental staff is here to take care of your smile and needs.
+          </p>
+        </Fade>
+    
       </article>
-      
+
     </div>
     </>
   ) 
