168b7546c13280c300ef708be4752e41b8610eee
Sue Roberts <suerobertstx@yahoo.com>
2022-12-10 11:52:53 -0600

    Style & move email button

--- dentist/src/components/header/Header.jsx
+++ dentist/src/components/header/Header.jsx
@@ -4,6 +4,7 @@ import headerImg from "../../assets/images/smile1.jpg";
 import { FiCalendar } from 'react-icons/fi'
 import { BsTelephone } from 'react-icons/bs'
 import {FaTooth } from 'react-icons/fa'
+import {HiOutlineMail } from 'react-icons/hi'
 import './header.scss'
 
 
@@ -11,12 +12,23 @@ const Header = () => {
   return (
     <section className="section-1 header__container section__padding">
       <main className="intro-page">
-        
+        {/* Heading */}
           <h1>Tillamook Family Dentistry</h1> 
           <h2>2503 Main Ave N, Tillamook, OR 97141
           </h2>
-          <h4><FiCalendar className="office-info"/> MON - THURS: 9:00 AM - 5:00 PM <FaTooth className="office-info"/> AVAILABLE FOR EMERGENCIES <BsTelephone className="office-info"/> 503-815-1777</h4>
+
+        {/* Office info */}
+          <h4><FiCalendar className="office-info"/> MON - THURS: 9:00 AM - 5:00 PM <FaTooth className="office-info"/> AVAILABLE FOR EMERGENCIES <BsTelephone className="office-info"/> 503-815-1777 <HiOutlineMail className="office-info"/> </h4>
           
+          <Link to="/contact" className="contact-button-home">
+        <div>
+            <span className="bg switch__bg"></span>
+            <span className="base switch__border-color"></span>
+            <span className="text">Send Email</span>
+            </div> 
+          </Link>
+        
+
           <img src={ headerImg } className="headerImg" alt="Dentist office  logo" width='60%'/>
 
           
@@ -28,13 +40,13 @@ const Header = () => {
           {/* <img src={tempImage} alt="temp" width='55%' className="tempImg"/> */}
        
 
-        <Link to="/contact" className="contact-button">
+        {/* <Link to="/contact" className="contact-button-home">
           <div>
             <span className="bg switch__bg"></span>
             <span className="base switch__border-color"></span>
             <span className="text">Contact Us</span>
           </div>
-        </Link>
+        </Link> */}
       </main>
     
     </section>
